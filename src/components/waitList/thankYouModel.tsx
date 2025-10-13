b848de3ff05b55794d1c5868b134cb67e64b814c
Bhinsar2 <bhinsar.jagdish@blinkkaro.com>
2025-10-13 20:07:40 +0530

    feat: add FloatingButton component and ThankYouModel for waitlist confirmation
    - Implemented FloatingButton that links to the waitlist, hidden on the waitlist page.
    - Created ThankYouModel component to display a confirmation message after joining the waitlist, with options to explore services or return to the homepage.

---
+++ src/components/waitList/thankYouModel.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { CheckIcon } from 'lucide-react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation' // Use next/navigation for App Router
+import React from 'react'
+
+const ThankYouModel = ({ onclose }) => {
+  const router = useRouter()
+
+  const goToServices = () => {
+    router.push('/services')
+  }
+
+  const handleModalContentClick = (e) => {
+    e.stopPropagation()
+  }
+
+  return (
+    <div
+      className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 p-4'
+      onClick={onclose}
+    >
+      <div
+        onClick={handleModalContentClick}
+        className='w-150 rounded-lg bg-white p-8 shadow-2xl dark:bg-gray-800'
+      >
+         <CheckIcon className='text-green-700/80 w-30 h-40 mx-auto mb-10' />
+
+        <h2 className='mt-6 text-4xl font-bold text-gray-900 dark:text-gray-100'>
+          Thank You!
+        </h2>
+
+        <p className='mt-2 text-base text-gray-600 dark:text-gray-300'>
+          We've successfully added you to the waitlist for Kart Square. You'll be among the first to know when we launch! Stay connected with us for updates and sneak peeks
+        </p>
+
+        <button
+          onClick={goToServices}
+          className='mt-6  rounded-full bg-green-600 px-6 py-3 text-base font-semibold text-white transition-colors duration-300 hover:bg-green-700 dark:bg-green-700 dark:hover:bg-green-800'
+        >
+          Explore Our Services
+        </button>
+
+        <div className='mt-4 text-sm text-gray-500 dark:text-gray-400'>
+          Or, return to the{' '}
+          <Link
+            href='/'
+            className='underline text-blue-500 transition-colors hover:text-blue-600'
+          >
+            Homepage
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ThankYouModel
