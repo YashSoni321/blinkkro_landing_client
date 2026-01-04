99f8f7e24939fa2076f3959a1839b92e2596978f
Yash soni <yashsoni@heyneo.so>
2026-01-04 11:51:20 +0530

    changes in landing page

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,11 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  // Block all crawlers during development/staging
   return {
     rules: {
-      userAgent: '*',
-      disallow: '/', // Block all pages
+      userAgent: "*",
+      allow: "/", // Allow all pages
     },
-    sitemap: 'https://kartsquare.com/sitemap.xml',
-  }
+    sitemap: "https://kartsquare.com/sitemap.xml",
+  };
 }
